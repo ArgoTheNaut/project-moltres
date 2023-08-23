e5c740260bdf1cca872725be3c17570787e95fbd
Argo <36687789+ArgoTheNaut@users.noreply.github.com>
2023-08-22 20:05:13 -0400

    autoformat consequences

--- bot.py
+++ bot.py
@@ -1,41 +1,46 @@
 # Built from quickstart
 # https://discordpy.readthedocs.io/en/stable/quickstart.html
+# https://github.com/adafruit/Adafruit_CircuitPython_MCP9808
 import os
 import discord
+import board
+import adafruit_mcp9808
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-CHANNELS = {
-    'stdout': 1134634193998065745,
-    'stderr': 1134634212390084628
-}
+CHANNELS = {"stdout": 1134634193998065745, "stderr": 1134634212390084628}
 
 client = discord.Client(intents=intents)
 
+
 async def send_status_update_normal(info: str):
-    channel = client.get_channel(CHANNELS['stdout'])
+    channel = client.get_channel(CHANNELS["stdout"])
     print("Retrieved channel:", channel)
     await channel.send(info)
 
+
 async def send_warning(info: str):
-    channel = client.get_channel(CHANNELS['stderr'])
+    channel = client.get_channel(CHANNELS["stderr"])
     print("Retrieved channel:", channel)
     await channel.send(info)
+
 
 @client.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')
+    print(f"We have logged in as {client.user}")
     await send_status_update_normal("Standard Message")
     await send_warning("Standard warning test")
+
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+    if message.content.startswith("$hello"):
+        await message.channel.send("Hello!")
+
 
 with open("token.txt", "r") as auth_token:
     client.run(auth_token.read())
